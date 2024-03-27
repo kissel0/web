c56dea88d90736b39f529292854773fe2c58d0ae
kisel <kiselevaeva377@gmail.com>
2024-03-27 23:49:09 +0300

    Результаты отбора

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ def func():
 def index():
     return "И на Марсе будут яблони цвести!"
 
+
 @app.route('/promotion')
 def promotion():
     return f'''<!doctype html>
@@ -47,6 +48,7 @@ def promotion():
                   </body>
                 </html>'''
 
+
 @app.route('/choice/<planet_name>')
 def option(planet_name):
     return f'''<!doctype html>
@@ -77,6 +79,34 @@ def option(planet_name):
                         </div>
                       </body>
                     </html>'''
+
+
+@app.route('/results/<nickname>/<int:level>/<float:rating>')
+def result(nickname, level, rating):
+    return f'''<!doctype html>
+                    <html lang="en">
+                      <head>
+                        <meta charset="utf-8">
+                        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                        <link rel="stylesheet"
+                       href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                       integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                       crossorigin="anonymous">
+                        <title>Пример с несколькими параметрами</title>
+                      </head>
+                      <body>
+                        <h1>Результаты отбора</h1>
+                        <h2>Претендента на участие в миссии {nickname}:</h2>
+                        <div class="alert alert-success" role="alert">
+                            <h2>Поздравляем! Ваш рейтинг после {level} этапа отбора</h2>
+                        </div>
+                        <h4>составляет {rating}!</h4>
+                        <div class="alert alert-warning" role="alert">
+                            <h2>Желаем удачи!</h2>
+                        </div>
+                      </body>
+                    </html>'''
+
 
 @app.route('/image_mars')
 def image():
