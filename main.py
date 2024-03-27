ce2546267b484f46867d58ed5642ecb32401f781
kisel <kiselevaeva377@gmail.com>
2024-03-27 23:03:35 +0300

    Варианты выбора

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ def func():
 def index():
     return "И на Марсе будут яблони цвести!"
 
-
 @app.route('/promotion')
 def promotion():
     return f'''<!doctype html>
@@ -27,7 +26,7 @@ def promotion():
                     <title>Колонизация</title>
                   </head>
                   <body><link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
-                  
+
                     <h1>Жди нас, Марс!<h1>
                     <img src="{url_for('static', filename='img/mars.png')}">
                     <div class="alert alert-secondary" role="alert">
@@ -48,6 +47,36 @@ def promotion():
                   </body>
                 </html>'''
 
+@app.route('/choice/<planet_name>')
+def option(planet_name):
+    return f'''<!doctype html>
+                    <html lang="en">
+                      <head>
+                        <meta charset="utf-8">
+                        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                       <link rel="stylesheet"
+                       href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                       integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                       crossorigin="anonymous">
+                        <title>Варианты выбора</title>
+                      </head>
+                      <body>
+                        <h1>Мое предложение:{planet_name}</h1>
+                        <h2>Эта планета близка к Земле;</h2>
+                        <div class="alert alert-success" role="alert">
+                            На ней много необходимых ресурсов;
+                        </div>
+                        <div class="alert alert-secondary" role="alert">
+                            На ней есть вода и атмосфера;
+                        </div>
+                        <div class="alert alert-warning" role="alert">
+                            На ней есть небольшое магнитное поле;
+                        </div>
+                        <div class="alert alert-danger" role="alert">
+                            Наконец, она просто красива!;
+                        </div>
+                      </body>
+                    </html>'''
 
 @app.route('/image_mars')
 def image():
